885d546411cf5f7383000f5c676366194f679022
JeevithasHetty <jeevitha3shetty@gmail.com>
2026-06-24 23:55:54 +0530

    Fix API base URL

--- frontend/src/api/listings.js
+++ frontend/src/api/listings.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/listings",
+  baseURL: `${import.meta.env.VITE_API_URL}/listings`,
 });
 
 API.interceptors.request.use((req) => {
