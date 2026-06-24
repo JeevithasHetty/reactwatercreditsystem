12e7457d6003a4163ffd821b722eea70bfbb804a
JeevithasHetty <jeevitha3shetty@gmail.com>
2026-06-24 19:26:40 +0530

    Fix backend routes and controllers

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const r = express.Router();
-const { signup, login, getMe } = require('../controllers/authController');
+
+const authController = require('../controllers/authController');
+console.log('AUTH CONTROLLER:', authController);
+
+const { signup, login, getMe } = authController;
+
 const { protect } = require('../middleware/auth');
 
 r.post('/signup', signup);
-r.post('/login',  login);
-r.get('/me',      protect, getMe);
+r.post('/login', login);
+r.get('/me', protect, getMe);
 
 module.exports = r;
