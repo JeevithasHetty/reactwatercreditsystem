f3601fa76325ee5f702e828b7beedd92820af3ba
JeevithasHetty <jeevitha3shetty@gmail.com>
2026-06-24 19:04:44 +0530

    Fix auth controller exports

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+
 const authController = require("../controllers/authController");
+https://github.com/JeevithasHetty/reactwatercreditsystem
 
 // ✅ make sure these exist in authController.js: exports.signup, exports.login
 router.post("/signup", authController.signup);
